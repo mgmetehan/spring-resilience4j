4b415cb2b511cd41c8f3cd0719a20b0704c01fbd
mgmetehan <mgmetehan@hotmail.com>
2024-06-02 17:42:49 +0300

    init project

---
+++ src/main/java/com/mgmetehan/springresilience4j/SpringResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.springresilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResilience4jApplication.class, args);
+	}
+
+}
